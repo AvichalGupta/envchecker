41b950a4e1771fd2f4b6597893910635a0bb14af
Avichal Gupta <avichalgupta1999@gmail.com>
2022-04-12 23:45:08 +0530

    improved code quality

--- src/extension.ts
+++ src/extension.ts
@@ -1,18 +1,11 @@
 import * as vscode from 'vscode';
-const acceptableCharachters: { [key: string]: string; } = {_:'_',a:'a',b:'b',c:'c',d:'d',e:'e',f:'f',g:'g',h:'h',i:'i',j:'j',k:'k',l:'l',m:'m',n:'n',o:'o',p:'p',q:'q',r:'r',s:'s',t:'t',u:'u',v:'v',w:'w',x:'x',y:'y',z:'z'};
+import * as moment from 'moment';
+const acceptableCharachters: { [key: string]: string; } = {_:'_',$:'$',a:'a',b:'b',c:'c',d:'d',e:'e',f:'f',g:'g',h:'h',i:'i',j:'j',k:'k',l:'l',m:'m',n:'n',o:'o',p:'p',q:'q',r:'r',s:'s',t:'t',u:'u',v:'v',w:'w',x:'x',y:'y',z:'z'};
 
-function checkForAcceptableCharachters (str: string) {
-	console.log(str);
-	let char = str[str.length-1];
-	console.log(char);
-	if(acceptableCharachters[char.toLowerCase()]) {
-		return str.slice(1);
-	}
-	return str.slice(1,str.length-1);
-}
-
+// ',',')',';',' ','[',']','=','+','-','*','/','^','%'
 export function activate(context: vscode.ExtensionContext) {
-	let disposable = vscode.commands.registerCommand('envchecker.helloWorld', async() => {
+	let disposable = vscode.commands.registerCommand('envchecker.checkFile', async() => {
+		const startTime = moment.now();
 		if(vscode.workspace.workspaceFolders) {
 			let doc = await vscode.workspace.openTextDocument(vscode.workspace.workspaceFolders[0].uri.path + "/.env");
 			let envData,envFileKey,envFileKeysArr=new Set();
@@ -23,25 +16,52 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 			if(vscode.window.activeTextEditor) {
 				let currentDoc = await vscode.workspace.openTextDocument(vscode.window.activeTextEditor.document.fileName);
-				let envKeyInActiveFile: string;
-				let currentLine,splitLineData;
-				console.log(currentDoc.getText().split('process.env'));
-				for(let i=0;i<currentDoc.lineCount;i++) {
-					currentLine = currentDoc.lineAt(i).text;
-					if(currentLine.includes('process.env')) {
-						splitLineData = currentLine.toString().split('process.env');
-						for(let j=0;j<splitLineData.length;j++) {
-							if(splitLineData[j].includes('.')) { 
-								envKeyInActiveFile = checkForAcceptableCharachters(splitLineData[j]);
-								if(!envFileKeysArr.has(envKeyInActiveFile)) {
-									vscode.window.showErrorMessage(`Key ${envKeyInActiveFile} is not present in .env file`);
-								}
-							}
+				let envKeyIndexInActiveFile = 0;
+				let currentDocText = currentDoc.getText();
+				let occurenceIndex;
+				let occurenceIndices: number[] = [];
+				let startIndex = 0;
+				let searchText = 'process.env';
+				while((occurenceIndex = currentDocText.indexOf('process.env',startIndex))>-1) {
+					envKeyIndexInActiveFile = occurenceIndex+searchText.length+1;
+					occurenceIndices.push(envKeyIndexInActiveFile);
+					startIndex = occurenceIndex+searchText.length;
+					console.log(currentDocText[envKeyIndexInActiveFile]);
+				}
+				// console.log('occurenceIndices',occurenceIndices);
+				let i = occurenceIndices[0];
+				let envKeyInActiveFile = '';
+				let m=0;
+				let excludedKeysInActiveFile = new Set<string>();
+				while(i<currentDocText.length) {
+					if(acceptableCharachters[currentDocText[i].toLowerCase()]){
+						envKeyInActiveFile+=currentDocText[i];
+						i++;
+					} else {
+						if(!envFileKeysArr.has(envKeyInActiveFile)){
+							excludedKeysInActiveFile.add(envKeyInActiveFile);
 						}
+						envKeyInActiveFile='';
+						m+=1;
+						i=occurenceIndices[m];
 					}
 				}
+				// console.log('excludedKeysInActiveFile: ',excludedKeysInActiveFile);
+				let finalExcludedKeysObj = '';
+				if(excludedKeysInActiveFile.size) {
+					for(let i=0;i<excludedKeysInActiveFile.size;i+=1) {
+						finalExcludedKeysObj+=`${[...excludedKeysInActiveFile][i]}, \n`;
+					}
+					vscode.window.showErrorMessage(`The following keys are not present in your .env file: \n${finalExcludedKeysObj}`);
+				}
+			} else {
+				vscode.window.showErrorMessage('Please open any file to use ENVChecker.');
 			}
+		} else {
+			vscode.window.showErrorMessage('Please open any folder to use ENVChecker.');
 		}
+		const stopTime = moment.now();
+		console.log('execution time: ', (stopTime-startTime)/1000, 'seconds');
 	});
 	context.subscriptions.push(disposable);
 }
